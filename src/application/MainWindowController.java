d739d6b4c232a941fa0ce85c930e0fcb81df3738
George Alves da Silveira <georgealvessilveira@outlook.com>
2015-12-04 19:30:36 -0200

    primeitro commit

---
+++ src/application/MainWindowController.java
@@ -0,0 +1,22 @@
+package application;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class MainWindowController {
+
+	@FXML private Label label;
+	@FXML private TextField field;
+	
+	private Main main;
+	
+	public void setMain(Main main) {
+		this.main = main;
+	}
+	
+	public void handleButton() {
+		String text = field.getText();
+		this.label.setText(text);
+	}
+}
